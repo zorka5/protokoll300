f74d9a1996cd0e57e7426b327174a0c09c98d229
Zofia Kubrak <zofiakubrak@gmail.com>
2022-10-06 22:45:22 +0200

    add docstring

--- protokoll300/frame.py
+++ protokoll300/frame.py
@@ -1,7 +1,9 @@
 class Frame:
-    _frame_sequence: bytearray
-    _frame_list: list[int]
+    """
+    Class representing standard protokoll300 frame.
+    """
 
+    _frame_sequence: bytearray
     _start_byte: bytes
     _frame_length: bytes
     _unit_identifier: bytes
@@ -11,29 +13,41 @@ class Frame:
 
     def __init__(self, frame_string: str) -> None:
         self._frame_sequence = bytearray.fromhex(frame_string)
-        self._frame_list = list(self._frame_sequence)
         self._start_byte = self._frame_sequence[0].to_bytes(1, "big")
         self._frame_length = self._frame_sequence[1].to_bytes(1, "big")
         self._unit_identifier = self._frame_sequence[2].to_bytes(1, "big")
         self._function_code = self._frame_sequence[3].to_bytes(1, "big")
-        self._address = bytearray([self._frame_sequence[4], self._frame_sequence[5]])
+        self._address = bytearray(
+            [
+                self._frame_sequence[4],
+                self._frame_sequence[5],
+            ]
+        )
         self._control_sum = int.to_bytes(self.calculate_checksum(), 1, "little")
+        self._frame_sequence += self._control_sum
 
     def calculate_checksum(self) -> int:
+        """
+        Method used to calculate checksum by adding bytes from second to last,
+        determined by frame length property.
+        """
         frame_length = int.from_bytes(self._frame_length, "little")
         dec = list(self._frame_sequence[1 : frame_length + 2])
         return sum(dec) % 256
 
     @property
     def sequence(self) -> bytearray:
+        "Property of frame byte sequence"
         return self._frame_sequence
 
     @property
     def checksum(self) -> int:
+        "Property of frame checksum"
         return self.calculate_checksum()
 
 
 class RequestFrame(Frame):
+    "Class representing request frame"
     _expected_data_length: int
 
     def __init__(
@@ -62,7 +76,11 @@ class RequestFrame(Frame):
         super().__init__(frame_string=frame_bytes.hex())
 
 
+# pylint: disable=pylint(too-many-arguments)
+
+
 class ResponseFrame(Frame):
+    "class representing response frame"
     _data_length: int
     _data: list[bytes]
 
