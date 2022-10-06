6680ee3f9a55ae2d57a79a9befbc7f573947dbee
Zofia Kubrak <zofiakubrak@gmail.com>
2022-10-06 22:06:35 +0200

    chcnage isolate_frame function

--- protokoll300/utils.py
+++ protokoll300/utils.py
@@ -1,13 +1,24 @@
 from typing import Optional
 
 
-def isolate_frame(frame_str: str) -> Optional[list[str]]:
-    frame_list = frame_str.strip().split(" ")
+def isolate_frame(frame: bytes, start_byte: bytes) -> Optional[bytes]:
+    """
+    Function isolating frame from given bytes.
+    Function searches for starting byte and returns frame starting with it.
+    Params:
+        -frame - given frame to isolate
+        -start_byte - given start byte
+    Return:
+        isolated frame or None if not found
+    """
+    frame_str = frame.decode("utf-8")
     try:
-        frame_list = frame_list[frame_list.index("41") :]
-        return frame_list
+        frame_str = frame_str[frame_str.index(start_byte.decode("utf-8")) :]
+        return frame_str.encode("utf-8")
     except ValueError:
         print("no start byte in frame")
+    return None
+
 
 def bytes_list_to_frame_string(bytes_list: list[bytes]) -> str:
     frame_str = ""
@@ -18,6 +29,7 @@ def bytes_list_to_frame_string(bytes_list: list[bytes]) -> str:
             hex_decimal_string = "0" + hex_decimal_string
         frame_str += hex_decimal_string + " "
     return frame_str
+
 
 def string_to_bytes_list(frame_str: str) -> list[bytes]:
     return list(map(lambda x: bytes.fromhex(x), frame_str.strip().split(" ")))
