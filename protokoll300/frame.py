f26b6132ed50d322935f3ca638fd219ce9f8d3ae
Zofia Kubrak <zofiakubrak@gmail.com>
2022-10-06 20:15:00 +0200

    delete checksum calculation

--- protokoll300/frame.py
+++ protokoll300/frame.py
@@ -1,29 +1,3 @@
-from .utils import sum_list_bytes
-
-"""
-Szczegóły telegramu:
-Kiedy dane są odczytywane :
-    0x41Bajt początkowy telegramu
-    0xXXDługość danych użytkownika jako liczba bajtów między bajtem początkowym telegramu (0x41) a sumą kontrolną)
-    0x0XMessageIdentifier (niższe 4 bity): 0x00 = żądanie, 0x01 = odpowiedź, 0x02 = niepotwierdzone, 0x03 = błąd
-    0x0XFunctionCode (mniejsze 5 bitów): 0x01 = Virtual_READ, 0x02 = Virtual_WRITE, 0x07 = Remote_Procedure_Call
-    Bity 5-7 to numer sekwencyjny komunikatu, który jest również ustawiany w odpowiedzi.
-    0xXX 0xXX2-bajtowy adres danych lub procedury
-    0xXXOczekiwana liczba bajtów w odpowiedzi
-    0xXXSuma kontrolna = suma wartości z 2 bajtu (bez 0x41)
-Jeżeli dane mają zostać zapisane:
-    0x41Bajt początkowy telegramu
-    0xXXDługość danych użytkownika (liczba bajtów między bajtem początkowym telegramu (0x41) a sumą kontrolną)
-    0x0X0x00 = żądanie, 01 = odpowiedź, 03 = błąd
-    0x0X0x01 = ReadData, 0x02 = WriteData, 07 = Function Call
-    Bits 7,6,5 można ustawić dowolnie, są one również ustawiane w odpowiedzi
-    0xXX 0xXX2-bajtowy adres danych lub procedury
-    0xXXLiczba bajtów do zapisania w
-    0xXX... treść do napisania
-    0xXXSuma kontrolna = suma wartości z 2 bajtu (bez 0x41)
-"""
-
-
 class Frame:
     _frame_sequence: bytearray
     _frame_list: list[int]
@@ -84,10 +58,6 @@ class RequestFrame(Frame):
 
         length_byte = (len(frame_list) - 1).to_bytes(1, "little")
         frame_list.insert(1, length_byte)
-
-        cs_byte = sum_list_bytes(frame_list)
-        frame_list.append(cs_byte)
-
         frame_bytes = b"".join(frame_list)
         super().__init__(frame_string=frame_bytes.hex())
 
