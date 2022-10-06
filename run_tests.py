39dfec40f2399a1ab379ecc8c9d235c8f135ce31
Zofia Kubrak <zofiakubrak@gmail.com>
2022-10-06 22:20:43 +0200

    add frame testing

---
+++ run_tests.py
@@ -0,0 +1,13 @@
+from tests.tests_utils import test_isolate_frame
+from tests.tests_frame import (
+    test_request_frame,
+    test_response_frame,
+    test_request_frame_current_params,
+    test_frame_checksum,
+)
+
+test_isolate_frame()
+test_request_frame()
+test_request_frame_current_params()
+test_response_frame()
+test_frame_checksum()
