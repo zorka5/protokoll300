8328423ff9fb9bf2790298d4c48b0e7cd46be8de
zorka5 <zofiakubrak@gmail.com>
2022-10-09 15:26:23 +0200

    delete response and request frame

--- protokoll300/frame.py
+++ protokoll300/frame.py
@@ -1,108 +1,39 @@
+from dataclasses import dataclass
+from typing import Optional
+
+
+@dataclass
 class Frame:
     """
     Class representing standard protokoll300 frame.
     """
 
-    _frame_sequence: bytearray
-    _start_byte: bytes
-    _frame_length: bytes
-    _unit_identifier: bytes
-    _function_code: bytes
-    _address: bytearray
-    _control_sum: bytes
+    frame_sequence: bytes
+    start_byte: bytes
+    frame_length: bytes
+    unit_identifier: bytes
+    function_code: bytes
+    address: bytes
+    data_length: bytes
+    data: bytes
+    control_sum: bytes
 
-    def __init__(self, frame_string: str) -> None:
-        self._frame_sequence = bytearray.fromhex(frame_string)
-        self._start_byte = self._frame_sequence[0].to_bytes(1, "little")
-        self._frame_length = self._frame_sequence[1].to_bytes(1, "little")
-        self._unit_identifier = self._frame_sequence[2].to_bytes(1, "little")
-        self._function_code = self._frame_sequence[3].to_bytes(1, "little")
-        self._address = bytearray(
-            [
-                self._frame_sequence[4],
-                self._frame_sequence[5],
-            ]
-        )
-        self._control_sum = int.to_bytes(self.calculate_checksum(), 1, "little")
-        self._frame_sequence += self._control_sum
+    def __init__(self, frame_bytes: bytes) -> None:
+        self.frame_sequence = frame_bytes
+        self.start_byte = frame_bytes[0].to_bytes(1, "little")
+        self.frame_length = frame_bytes[1].to_bytes(1, "little")
+        self.unit_identifier = frame_bytes[2].to_bytes(1, "little")
+        self.function_code = frame_bytes[3].to_bytes(1, "little")
+        self.address = frame_bytes[4:6]
+        self.data_length = frame_bytes[6].to_bytes(1, "little")
+        self.data = frame_bytes[7:][:-1]
+        self.control_sum = frame_bytes[-1].to_bytes(1, "little")
 
-    def calculate_checksum(self) -> int:
+    def is_ok(self) -> bool:
         """
         Method used to calculate checksum by adding bytes from second to last,
         determined by frame length property.
         """
-        frame_length = int.from_bytes(self._frame_length, "little")
-        dec = list(self._frame_sequence[1 : frame_length + 2])
-        return sum(dec) % 256
-
-    @property
-    def sequence(self) -> bytearray:
-        "Property of frame byte sequence"
-        return self._frame_sequence
-
-    @property
-    def checksum(self) -> bytes:
-        "Property of frame checksum"
-        return self._control_sum
-
-
-class ResponseFrame(Frame):
-    "Class representing request frame"
-    _expected_data_length: int
-
-    def __init__(
-        self,
-        start_byte: bytes,
-        unit_identifier: bytes,
-        function_code: bytes,
-        procedure_adress_and_length: tuple[bytes, bytes, int],
-    ) -> None:
-        self._expected_data_length = procedure_adress_and_length[2]
-
-        data_length_bytes = procedure_adress_and_length[2].to_bytes(1, "little")
-
-        frame_list = [
-            start_byte,
-            unit_identifier,
-            function_code,
-            procedure_adress_and_length[0],
-            procedure_adress_and_length[1],
-            data_length_bytes,
-        ]
-
-        length_byte = (len(frame_list) - 1).to_bytes(1, "little")
-        frame_list.insert(1, length_byte)
-        frame_bytes = b"".join(frame_list)
-        super().__init__(frame_string=frame_bytes.hex())
-
-
-# pylint: disable=pylint(too-many-arguments)
-
-
-class RequestFrame(Frame):
-    "class representing request frame"
-    _data_length: int
-    _data: list[bytes]
-
-    def __init__(
-        self,
-        start_byte: bytes,
-        unit_identifier: bytes,
-        function_code: bytes,
-        procedure_adress_and_length: tuple[bytes, bytes, int],
-        data: list[bytes],
-    ) -> None:
-        self._data_length = procedure_adress_and_length[2]
-        self._data = data
-        frame_list = [
-            start_byte,
-            unit_identifier,
-            function_code,
-            procedure_adress_and_length[0],
-            procedure_adress_and_length[1],
-            procedure_adress_and_length[2].to_bytes(1, "little"),
-        ]
-        frame_list += data
-        frame_list.insert(1, (len(frame_list) - 1).to_bytes(1, "little"))
-        frame_bytes = b"".join(frame_list)
-        super().__init__(frame_string=frame_bytes.hex())
+        frame_length = int.from_bytes(self.frame_length, "little")
+        dec = list(self.frame_sequence[1 : frame_length + 2])
+        return sum(dec) % 256 == int.from_bytes(self.control_sum, "little")
--- test/test_frame/test_frame.py
+++ test/test_frame/test_frame.py
@@ -8,6 +8,40 @@ from protokoll300.frame import Frame
 
 
 class TestFrame(unittest.TestCase):
-    def test_frame_checksum(self):
-        frame = Frame("41 05 00 01 08 0C 10 2A")
-        self.assertEqual(frame.checksum, b"\x2A")
+    def test_frame_current_params_request(self):
+        frame = Frame(b"\x41\x05\x00\x01\x08\x0C\x10\x2A")
+        self.assertEqual(frame.start_byte, b"\x41")
+        self.assertEqual(frame.frame_length, b"\x05")
+        self.assertEqual(frame.unit_identifier, b"\x00")
+        self.assertEqual(frame.function_code, b"\x01")
+        self.assertEqual(frame.address, b"\x08\x0C")
+        self.assertEqual(frame.data_length, b"\x10")
+        self.assertEqual(frame.data, b"")
+        self.assertEqual(frame.control_sum, b"\x2A")
+        self.assertTrue(frame.is_ok())
+
+    def test_frame_current_params_response(self):
+        frame = Frame(
+            b"\x41\x15\x01\x01\x08\x0c\x10\x6c\x01\x7b\x00\x1d\x01\xc6\x01\xc8\x00\x4f\x01\xc8\x00\x6d\x01\x56"
+        )
+        self.assertEqual(frame.start_byte, b"\x41")
+        self.assertEqual(frame.frame_length, b"\x15")
+        self.assertEqual(frame.unit_identifier, b"\x01")
+        self.assertEqual(frame.function_code, b"\x01")
+        self.assertEqual(frame.address, b"\x08\x0C")
+        self.assertEqual(frame.data_length, b"\x10")
+        self.assertEqual(
+            frame.data,
+            b"\x6c\x01\x7b\x00\x1d\x01\xc6\x01\xc8\x00\x4f\x01\xc8\x00\x6d\x01",
+        )
+        self.assertEqual(frame.control_sum, b"\x56")
+        self.assertTrue(frame.is_ok())
+
+    def test_frame_outdoor_temp_request(self):
+        frame = Frame(b"\x41\x05\x00\x01\x55\x25\x04\x84")
+        self.assertEqual(frame.control_sum, b"\x84")
+        self.assertEqual(
+            frame.data,
+            b"",
+        )
+        self.assertTrue(frame.is_ok())
