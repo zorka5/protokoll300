ffe3b41fab56c76c3bc6bcd4df330f6e4aa1847d
zorka5 <zofiakubrak@gmail.com>
2022-10-09 09:45:19 +0200

    response rame tests added

---
+++ tests/tests_response_frame.py
@@ -0,0 +1,33 @@
+import unittest
+from protokoll300.frame import ResponseFrame
+from protokoll300.codes import (
+    UnitIdentifier,
+    FunctionCodes,
+    ProcedureAdressesAndExpectedLength,
+)
+
+
+class TestResponseFrame(unittest.TestCase):
+    "Class to test Request frames"
+
+    def test_response_frame_checksum(self):
+        "Method to check checksum calculation"
+        frame = ResponseFrame(
+            start_byte=b"\x41",
+            unit_identifier=UnitIdentifier.REQUEST.value,
+            function_code=FunctionCodes.VIRTUAL_WRITE.value,
+            procedure_adress_and_length=ProcedureAdressesAndExpectedLength.M2_REQUIRED_ROOM_TEMP_NORMAL_OPERATION,
+            data=[b"\x0D", b"\x00"],
+        )
+        self.assertEqual(frame.sequence, b"\x41\x07\x00\x02\x33\x06\x01\x0D\x00\x50")
+
+    def test_response_frame_checksum_one_byte(self):
+        "Method to check checksum calculation"
+        frame = ResponseFrame(
+            start_byte=b"\x41",
+            unit_identifier=UnitIdentifier.REQUEST.value,
+            function_code=FunctionCodes.VIRTUAL_WRITE.value,
+            procedure_adress_and_length=ProcedureAdressesAndExpectedLength.M2_REQUIRED_ROOM_TEMP_NORMAL_OPERATION,
+            data=[b"\x0D"],
+        )
+        self.assertEqual(frame.sequence, b"\x41\x06\x00\x02\x33\x06\x01\x0D\x4F")
