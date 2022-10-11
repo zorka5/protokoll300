1c9f7a8219f360c0315eaa793b565eeef9686824
Zofia Kubrak <zofiakubrak@gmail.com>
2022-10-11 10:07:13 +0200

    delete unnecessary function and imports

--- protokoll300/utils.py
+++ protokoll300/utils.py
@@ -1,12 +1,4 @@
 from typing import Optional
-from protokoll300.codes import (
-    START_BYTE,
-    UnitIdentifier,
-    FunctionCodes,
-    ProcedureAdresses,
-    ExpectedLength,
-)
-
 
 def isolate_frame(frame: bytes, start_byte: bytes) -> Optional[bytes]:
     """
@@ -31,40 +23,3 @@ def sum_bytes_mod_256(bytes_list: bytes) -> bytes:
     for byte in bytes_list:
         sum += byte
     return (sum % 256).to_bytes(1, "little")
-
-
-# def parse_data(
-#     start_byte: bytes,
-#     unit_identifier: UnitIdentifier,
-#     function_code: FunctionCodes,
-#     procedure_address: ProcedureAdresses,
-#     expected_length: ExpectedLength,
-#     data: bytes,
-# ) -> bytes:
-#     frame_length = (
-#         len(unit_identifier.value)
-#         + len(function_code.value)
-#         + len(procedure_address.value)
-#         + len(expected_length.value)
-#         + len(data)
-#     ).to_bytes(1, "little")
-
-#     bs = b"".join(
-#         [
-#             frame_length,
-#             unit_identifier.value,
-#             function_code.value,
-#             procedure_address.value,
-#             expected_length.value,
-#             data,
-#         ]
-#     )
-
-#     control_sum = sum_bytes_mod_256(bs)
-#     return b"".join(
-#         [
-#             start_byte,
-#             bs,
-#             control_sum,
-#         ]
-#     )
